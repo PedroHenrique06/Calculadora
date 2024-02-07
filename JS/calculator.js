44f05f8b331d9e5449510d89a6bc0e3d14185154
Pedro Henrique <pedrosales2806@gmail.com>
2024-02-07 15:51:06 -0300

    Implementação da funcionalidade que faz com que os operadores apareçam no visor

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,4 +1,7 @@
+let count = []
 const MAX_VISOR_CHAR = 9
+
+
 
 function AddNumber(num){
     document.getElementById("total").removeAttribute("hidden")
@@ -7,12 +10,34 @@ function AddNumber(num){
     }
 }
 
+function CalcAction(action) {
+    let currentNumber = document.getElementById("total").innerHTML
+
+    if(currentNumber.length === 0) { return }
+
+    count.push(Number(document.getElementById("total").innerHTML))
+
+    if(currentNumber.split('')[currentNumber.length - 1] == '.') {
+        document.getElementById("accumulator").removeAttribute("hidden")
+        document.getElementById("accumulator").innerHTML += `${document.getElementById("total").innerHTML}0 ${action}`
+    }
+    else {
+        document.getElementById("accumulator").removeAttribute("hidden")
+        document.getElementById("accumulator").innerHTML += `${document.getElementById("total").innerHTML} ${action}`
+    }
+
+    document.getElementById("total").innerHTML = ""
+
+    count.push(action)
+
+}
+
 function CleanCurrentEntry() {
     document.getElementById("total").innerHTML = ""
 }
 
 function CleanAll() { 
     document.getElementById("total").innerHTML = ""
-    document.getElementById("accumulation").innerHTML = ""
+    document.getElementById("accumulator").innerHTML = ""
     count = []
 } 
