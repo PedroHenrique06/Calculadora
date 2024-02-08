9e5f5bc370cd379df667a54253888e3c28a65f3e
Pedro Henrique <pedrosales2806@gmail.com>
2024-02-08 15:53:26 -0300

    Adição das funções que calculam o resultado. Adição de comentários nos códigos

--- JS/calculator.js
+++ JS/calculator.js
@@ -2,7 +2,7 @@ let count = []
 const MAX_VISOR_CHAR = 9
 
 
-
+// Adiciona o valor ao visor 
 function AddNumber(num){
     document.getElementById("total").removeAttribute("hidden")
     if (document.getElementById("total").innerHTML.length < MAX_VISOR_CHAR) {
@@ -10,6 +10,7 @@ function AddNumber(num){
     }
 }
 
+// Adiciona operação ao visor 
 function CalcAction(action) {
     let currentNumber = document.getElementById("total").innerHTML
 
@@ -32,10 +33,80 @@ function CalcAction(action) {
 
 }
 
+let saveAction = 0
+
+// Efetua as operações e exibe o resultado
+function Result() {
+    let currentAccum = document.getElementById("accumulator").innerHTML
+    let currentNumber = document.getElementById("total").innerHTML
+
+    // ???
+    if(currentAccum[currentAccum.length-1] === "=" && currentNumber.length > 0) { // 1 + =
+        document.getElementById("total").innerHTML = ProcessAction(Number(currentNumber), Number(currentNumber), saveAction).toString().substring(0, MAX_VISOR_CHAR)
+    }
+
+    if(count.length === 0) { return }
+
+    // Organiza a linha com os calculos
+    count.push(Number(document.getElementById("total").innerHTML))
+    document.getElementById("accumulator").innerHTML += ` ${document.getElementById("total").innerHTML} =`
+    ProcessResult()
+
+}
+
+function ProcessResult() {
+    let action = null
+    let current = null
+    let total = 0
+
+    // Se o último for um operador
+    if(isNaN(count[count.length-1])) {
+        count.pop
+    }
+    // AJUSTAR A LÓGICA, ELA FALHA NO SINAL DO MEIO 
+    count.forEach(n => {// 1 + 2 (+) 3 + 4
+        // Se for um número
+        if(!isNaN(n)) {
+            if(current == null) {
+                current = n
+            }
+            else {
+                total += ProcessAction(current, n, action)
+                current = null
+            }
+        }
+        // Se for um operador
+        else {
+            action = n
+            saveAction = n
+        }
+    })
+
+    // Caso o último número sobre sem par
+    if(current != null) {
+        total = ProcessAction(total, current, action)
+    }
+
+    document.getElementById("total").innerHTML = total.toString().substring(0, MAX_VISOR_CHAR)
+    count = []
+}
+
+// Realiza a operação
+function ProcessAction(num1, num2, action) {
+    switch(action) {
+        case '+': return num1 + num2
+        case '-': return num1 - num2
+        case 'x': return num1 * num2
+        case '/': return num1 / num2
+    }
+}
+
+// Limpa o valor atual presente no visor
 function CleanCurrentEntry() {
     document.getElementById("total").innerHTML = ""
 }
 
+// Limpa toda informação do visor e do histórico
 function CleanAll() { 
     document.getElementById("total").innerHTML = ""
     document.getElementById("accumulator").innerHTML = ""
